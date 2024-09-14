02ceacbc322a54314af10b05003ba3694c67a29e
mssiddiqui11 <manal.23b1541171@abes.ac.in>
2024-09-14 12:32:47 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// Api key: CG - HhbUi8uSb9PZHjsBc6mfrkbS
+
+// https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd"
+
+let bitcoin = document.getElementById('bitcoin');
+let dogecoin = document.getElementById('dogecoin');
+let ethereum = document.getElementById('ethereum');
+
+// var setting = {
+//     "async" : true, 
+//     "scrossDomain" : true,
+//     "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+//     "method" : "GET", 
+//     "headers" : {}
+// }
+
+// $.ajax(setting).done(function (response) {
+//     bitcoin.innerHTML = response.bitcoin.usd;
+//     dogecoin.innerHTML = response.dogecoin.usd;
+//     ethereum.innerHTML = response.ethereum.usd;
+// })
+
+const apiURL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd";
+
+//Using async & await 
+
+async function getCost(url) {
+    let data = await fetch(url);
+    let response = await data.json();
+    bitcoin.innerHTML = response.bitcoin.usd;
+    dogecoin.innerHTML = response.dogecoin.usd;
+    ethereum.innerHTML = response.ethereum.usd;
+}
+
+getCost(apiURL);
+
+
+function changeTheme()
+{
+    if($('#toggle-btn').is(":checked"))
+    {
+        $('body').removeClass('light')
+    }
+    else
+    {
+        $('body').addClass('light')
+    }
+}
